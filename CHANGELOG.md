e10a29ce61eb2dca47336d35a40735ebd11ec960
Stephan Tarulli <srt@tinychameleon.com>
2023-01-22 21:08:57 -0800

    Document internal version release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 ## [Unreleased]
 
-## [0.1.0] - 2023-01-18
+### [0.2.0] - 2023-01-22
 
-- Initial release
+- UUID v6, v7, and v8 implemented quickly
+- Some performance optimization done
+
+### [0.1.0] - 2023-01-18
+
+- Initial release without design considerations
--- benchmark.rb
+++ benchmark.rb
@@ -4,11 +4,29 @@ require "securerandom"
 require "benchmark/ips"
 require "uuid/next"
 
+class ConstantUuid < Uuid::Next::V8
+  class << self
+    def custom_a
+      1
+    end
+
+    def custom_b
+      2
+    end
+
+    def custom_c
+      3
+    end
+  end
+end
+
 Benchmark.ips do |b|
   b.report("stdlib") { SecureRandom.uuid }
   b.report("uuidv6") { Uuid::Next::V6.generate }
   b.report("uuidv6s") { Uuid::Next::V6.generate.to_s }
   b.report("uuidv7") { Uuid::Next::V7.generate }
   b.report("uuidv7s") { Uuid::Next::V7.generate.to_s }
+  b.report("uuidv8") { ConstantUuid.generate }
+  b.report("uuidv8s") { ConstantUuid.generate.to_s }
   b.compare!
 end
