0d1928b96ee6dbf948ce6beb8ba7b1c5750f6539
Stephan Tarulli <srt@tinychameleon.com>
2023-01-22 20:49:44 -0800

    Optimize UUID v7

--- lib/uuid/next/v7.rb
+++ lib/uuid/next/v7.rb
@@ -8,11 +8,26 @@ class Uuid::Next::V7 < Uuid::Next::AbstractUuid
     VARIANT = 0x2 << 62
 
     def generate
-      a, b = SecureRandom.bytes(10).unpack("S>Q>")
+      a, b = byte_pool
       ts = (Process.clock_gettime(:CLOCK_REALTIME, :millisecond) & 0xffffffffffff) << 80
-      new(ts | VERSION | ((a & 0xfff) << 64) | VARIANT | b & 0x3fffffffffffffff)
+      new(ts | VERSION | ((a & 0xfff) << 64) | VARIANT | (b & 0x3fffffffffffffff))
+    end
+
+    BUFFER_SIZE = 64
+    NEEDED_BYTES = BUFFER_SIZE * 10
+    UNPACK_FORMAT = "S>Q>" * BUFFER_SIZE
+
+    def byte_pool
+      refill_pool! if @pool.empty?
+      @pool.pop(2)
+    end
+
+    def refill_pool!
+      @pool = SecureRandom.bytes(NEEDED_BYTES).unpack(UNPACK_FORMAT)
     end
   end
+
+  refill_pool!
 
   def initialize(bytes)
     super()
--- test/uuid/next/test_v7.rb
+++ test/uuid/next/test_v7.rb
@@ -4,8 +4,10 @@ require "test_helper"
 
 class UuidNextV7Test < Minitest::Test
   def test_that_it_generates_a_proper_uuid_v7
-    env_stubs time: 1234567898765, bytes: "\x1\x2\x3\x4\x5\x6\x7\x8\x9\x0" do
-      assert_equal "011f71fb-268d-7102-8304-050607080900", ::Uuid::Next::V7.generate.to_s
+    env_stubs time: 1234567898765, bytes: "\x1\x2\x3\x4\x5\x6\x7\x8" * 80 do
+      ::Uuid::Next::V7.refill_pool!
+
+      assert_equal "011f71fb-268d-7708-8102-030405060708", ::Uuid::Next::V7.generate.to_s
     end
   end
 end
