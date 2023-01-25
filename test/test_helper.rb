52a8cecd66f99bda6c469155ab1886e0c9039858
Stephan Tarulli <srt@tinychameleon.com>
2023-01-25 13:04:26 -0800

    Add test support files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "uuid/next"
 require "uuid"
 
+require "support/overflowing_generator"
+
 require "minitest"
 require "minitest/benchmark" if ENV["BENCH"]
 require "minitest/autorun"
