02ff12a7abd05fb8edced0467184520adf69cfa5
Stephan Tarulli <srt@tinychameleon.com>
2023-01-25 17:12:34 -0800

    Remove old implementations

--- lib/uuid/next/constants.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "nil_uuid"
-require_relative "max_uuid"
-
-module Uuid::Next
-  NIL_UUID = NilUuid.new.freeze
-  MAX_UUID = MaxUuid.new.freeze
-end
--- lib/uuid/next/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Uuid
-  module Next
-    VERSION = "0.2.0"
-  end
-end
