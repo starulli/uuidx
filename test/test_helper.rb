bd42351bb274e09fc58e38958e7649bdcb061d42
Stephan Tarulli <srt@tinychameleon.com>
2023-01-25 17:13:22 -0800

    Remove unused code from test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,13 +9,3 @@ require "support/overflowing_generator"
 require "minitest"
 require "minitest/benchmark" if ENV["BENCH"]
 require "minitest/autorun"
-
-class Minitest::Test
-  def env_stubs(time: 0, bytes: "\x1\x2\x3\x4\x5\x6\x7\x8", &block)
-    Process.stub :clock_gettime, time do
-      SecureRandom.stub :bytes, bytes do
-        block.call
-      end
-    end
-  end
-end
