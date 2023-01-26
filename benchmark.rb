23bdff17ab053adacca4780d8f9d4b58e5b20e03
Stephan Tarulli <srt@tinychameleon.com>
2023-01-25 21:15:05 -0800

    Remove additional references to old implementation

--- benchmark.rb
+++ benchmark.rb
@@ -2,24 +2,7 @@
 
 require "securerandom"
 require "benchmark/ips"
-require "uuid/next"
 require "uuid"
-
-class ConstantUuid < Uuid::Next::V8
-  class << self
-    def custom_a
-      1
-    end
-
-    def custom_b
-      2
-    end
-
-    def custom_c
-      3
-    end
-  end
-end
 
 module ConstantGenerator
   def self.custom_a
@@ -39,11 +22,8 @@ Uuid::Version8.generator = ConstantGenerator
 
 Benchmark.ips do |b|
   b.report("stdlib") { SecureRandom.uuid }
-  b.report("uuidv6") { Uuid::Next::V6.generate }
-  b.report("uuidv7") { Uuid::Next::V7.generate }
-  b.report("uuidv8") { ConstantUuid.generate }
   b.report("uuid6") { Uuid::Version6.generate }
   b.report("uuid7") { Uuid::Version7.generate }
-  b.report("uuid8") { Uuid::Version8.generate }
+  # b.report("uuid8") { Uuid::Version8.generate }
   b.compare!
 end
