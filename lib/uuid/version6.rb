06e1dd291f50bf80980039c7d897ac625fa552b6
Stephan Tarulli <srt@tinychameleon.com>
2023-01-25 21:14:36 -0800

    Make UUID Version 6 thread-safe

--- lib/uuid/version6.rb
+++ lib/uuid/version6.rb
@@ -15,6 +15,7 @@ module Uuid
     VERSION = 0x6 << 76 # :nodoc:
     VARIANT = 0x2 << 62 # :nodoc:
     GREGORIAN_MICROSECOND_TENTHS = 122_192_928_000_000_000 # :nodoc:
+    @seq_lock = Mutex.new
 
     # Construct a UUID Version 6 Value.
     def self.generate
@@ -22,8 +23,8 @@ module Uuid
       ts_high_mid = ((ts >> 12) & 0xffffffffffff) << 80
       ts_low = (ts & 0xfff) << 64
 
-      @clock_sequence += 1
-      seq = (@clock_sequence & 0x3fff) << 48
+      seq = @seq_lock.synchronize { @clock_sequence += 1 }
+      seq = (seq & 0x3fff) << 48
 
       Value.new ts_high_mid | VERSION | ts_low | VARIANT | seq | @node_id
     end
@@ -33,7 +34,8 @@ module Uuid
     end
 
     def self.reset_clock_sequence!
-      @clock_sequence = SecureRandom.bytes(4).unpack1("L>")
+      val = SecureRandom.bytes(4).unpack1("L>")
+      @seq_lock.synchronize { @clock_sequence = val }
     end
 
     private_class_method :reset_node_id!, :reset_clock_sequence!
