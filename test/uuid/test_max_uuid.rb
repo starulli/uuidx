da018a9cf8d592b1945624fb4516ff10cd357f8c
Stephan Tarulli <srt@tinychameleon.com>
2023-02-10 15:05:32 -0800

    Migrate nil and max UUIDs to string output

--- test/uuid/test_max_uuid.rb
+++ test/uuid/test_max_uuid.rb
@@ -4,21 +4,21 @@ require "test_helper"
 
 class TestMaxUuid < Minitest::Test
   def test_uuid_is_correct
-    assert_equal "ffffffff-ffff-ffff-ffff-ffffffffffff", Uuid::Max.to_s
+    assert_equal "ffffffff-ffff-ffff-ffff-ffffffffffff", Uuid.max_uuid
   end
 
   def test_max_equals_max
-    assert_equal Uuid::Max, Uuid::Max
+    assert_equal Uuid.max_uuid, Uuid.max_uuid
   end
 
   def test_max_is_greater_than_other_uuids
     gen6 = Uuid::Version6.new
     gen7 = Uuid::Version7.new
-    gen8 = OverflowingV8Generator.new
+    gen8 = Uuid::Version8.new(OverflowingGeneratorDefinition)
 
-    assert Uuid::Max > Uuid::Nil
-    100_000.times { assert Uuid::Max > gen6.generate }
-    100_000.times { assert Uuid::Max > gen7.generate }
-    100_000.times { assert Uuid::Max > gen8.generate }
+    assert Uuid.max_uuid > Uuid.nil_uuid
+    100_000.times { assert Uuid.max_uuid > gen6.generate }
+    100_000.times { assert Uuid.max_uuid > gen7.generate }
+    100_000.times { assert Uuid.max_uuid > gen8.generate }
   end
 end
--- test/uuid/test_nil_uuid.rb
+++ test/uuid/test_nil_uuid.rb
@@ -4,21 +4,21 @@ require "test_helper"
 
 class TestNilUuid < Minitest::Test
   def test_uuid_is_correct
-    assert_equal "00000000-0000-0000-0000-000000000000", Uuid::Nil.to_s
+    assert_equal "00000000-0000-0000-0000-000000000000", Uuid.nil_uuid
   end
 
   def test_nil_equals_nil
-    assert_equal Uuid::Nil, Uuid::Nil
+    assert_equal Uuid.nil_uuid, Uuid.nil_uuid
   end
 
   def test_nil_is_less_than_other_uuids
     gen6 = Uuid::Version6.new
     gen7 = Uuid::Version7.new
-    gen8 = OverflowingV8Generator.new
+    gen8 = Uuid::Version8.new(OverflowingGeneratorDefinition)
 
-    assert Uuid::Nil < Uuid::Max
-    100_000.times { assert Uuid::Nil < gen6.generate }
-    100_000.times { assert Uuid::Nil < gen7.generate }
-    100_000.times { assert Uuid::Nil < gen8.generate }
+    assert Uuid.nil_uuid < Uuid.max_uuid
+    100_000.times { assert Uuid.nil_uuid < gen6.generate }
+    100_000.times { assert Uuid.nil_uuid < gen7.generate }
+    100_000.times { assert Uuid.nil_uuid < gen8.generate }
   end
 end
